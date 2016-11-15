d7405d259249a04df26a0429ed5c6d2ea20bc224
Frederick Henderson <frederickjh@henderson-meier.org>
2016-11-15 01:12:36 +0100

    Fixed issue with MySQL setup not working on new environments. Everything use to be run as root but it was switched to the "docker" user. Now we need to tell it implicitly to run commands as the super user.

--- dockerfilescripts/run.sh
+++ dockerfilescripts/run.sh
@@ -78,9 +78,12 @@ cp  /.home-localdev/bin/* ~/bin/ 2>/dev/null
 # Reset home directory ownership
 gosu root chown $(id -u):$(id -g) -R ~
 
+# Reset webroot directory ownership
+gosu root chown $(id -u):$(id -g) /var/www
+
 # Check if the docroot exists, otherwise create it.
-if [[ ! -d $VOLUME_HOME/docroot ]]; then
-  mkdir -p $VOLUME_HOME/docroot
+if [[ ! -d /var/www/docroot ]]; then
+  mkdir -p /var/www/docroot
 fi
 
 # Below this is the original startup. Above are the commands brought in from the cli container.
@@ -89,10 +92,10 @@ VOLUME_HOME="/data"
 if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
     echo "=> Installing MySQL ..."
-    mysql_install_db > /dev/null 2>&1
+    gosu root mysql_install_db > /dev/null 2>&1
     echo "=> Done!"
-    /create_first_admin_user.sh
-    /create_database_and_users.sh
+    gosu root /create_first_admin_user.sh
+    gosu root /create_database_and_users.sh
 else
     echo "=> Using an existing volume of MySQL"
 fi
